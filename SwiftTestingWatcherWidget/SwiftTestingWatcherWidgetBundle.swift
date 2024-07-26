653ce874be35ae1267dbef65a04c76a7f92deb8e
Bartosz Chadryś <bartekch322@gmail.com>
2024-07-26 11:32:38 +0200

    Add widget extension

---
+++ SwiftTestingWatcherWidget/SwiftTestingWatcherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftTestingWatcherWidgetBundle.swift
+//  SwiftTestingWatcherWidget
+//
+//  Created by Bartek Chadryś on 26/07/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SwiftTestingWatcherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SwiftTestingWatcherWidget()
+    }
+}
