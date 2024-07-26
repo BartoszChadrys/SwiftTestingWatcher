11331c3fd8227c8a59d1be4a63a9be9ff2e2ba53
Bartosz Chadryś <bartekch322@gmail.com>
2024-07-26 11:29:52 +0200

    Initial Commit

---
+++ SwiftTestingWatcher/SwiftTestingWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingWatcherApp.swift
+//  SwiftTestingWatcher
+//
+//  Created by Bartek Chadryś on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
